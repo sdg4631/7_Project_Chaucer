fab7665f3b59f5620a845f73be8eaa7977018889
S&K <sdg4631@gmail.com>
2018-05-27 20:10:33 +0900

    Using Raycasts to Query Click

---
+++ 7_Project_Chaucer/Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour 
+{
+	CameraRaycaster cameraRaycaster;
+	
+	void Start()
+	{
+		cameraRaycaster = GetComponent<CameraRaycaster>();
+	}
+
+	void Update() 
+	{
+		var layerHit = cameraRaycaster.layerHit;
+		print(layerHit);
+	}
+}
