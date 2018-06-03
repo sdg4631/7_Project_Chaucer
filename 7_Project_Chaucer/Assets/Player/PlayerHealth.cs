e9ab60bbbd1042f32ed3d11ddcf220d2a4784cf7
S&K <sdg4631@gmail.com>
2018-06-03 14:29:44 +0900

    Using Interfaces in C#

--- 7_Project_Chaucer/Assets/Player/PlayerHealth.cs
+++ 7_Project_Chaucer/Assets/Player/PlayerHealth.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour 
+public class PlayerHealth : MonoBehaviour, IDamagable
 {
 	[SerializeField] public Stat health;
 
@@ -29,5 +29,10 @@ public class PlayerHealth : MonoBehaviour
 			health.CurrentVal += 10;
 		}
 		
+	}
+
+	public void TakeDamage(float damage)
+	{
+		health.CurrentVal = health.CurrentVal - damage;
 	}
 }
---
+++ 7_Project_Chaucer/Assets/Player/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		{
+			print("Collided with: " + other.gameObject.name);
+		}
+	}
+}
